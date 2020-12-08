35f9274f59db7ae7ee7a9285946ca4a48da66461
Debasis Saikia <debasishsaikia136@gmail.com>
2020-12-08 22:15:33 +0530

    'final-commit'

---
+++ scrapper.js
@@ -0,0 +1,27 @@
+const request = require("request"); //for url request ,we can also use axios
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+request(
+  "https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020",
+  (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+
+      //   common selector for span tag
+      const selectors = $(".lbl-licitacao");
+      //   console.log(selectors.html()); log the html tag of the element
+
+      // const output=selectors.find('span').text()
+      //   console.log(output);
+
+      //
+      $(".lbl-licitacao ").each((i, el) => {
+        const item = $(el).text();
+
+        //log all the the datas along with the values in a console
+        console.log(item);
+      });
+    }
+  }
+);
